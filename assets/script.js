1b764edf6974c58130e7c20633a85e54dc0e1455
Knitsy <131091408+Knitsy@users.noreply.github.com>
2023-05-25 23:36:16 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,143 +1,115 @@
-const questions = [
-    {
-      question: "Inside which HTML element do we put the JavaScript?",
-      choices: ["script", "scripting", "javascript", "js"],
-      answer: "script"
-    },
-    {
-      question: "Which programming language is used for web development?",
-      choices: ["JavaScript", "Python", "Java", "C++"],
-      answer: "JavaScript"
-    },
-    {
+// Quiz questions and answers
+const quizQuestions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript code?",
+    choices: ["<script>", "<javascript>", "<js>", "<scripting>"],
+    answer: "<script>"
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    choices: ["The <head> section", "The <body> section", "Both the <head> and <body> sections are correct"],
+    answer: "Both the <head> and <body> sections are correct"
+  },
+{
         question: "How do you write 'Hello World' in an alert box?",
         choices: ["msgBox", "alert", "msg", "alertbox"],
         answer: "alert"
       },
-  ];
-  
-  const quizContainer = document.getElementById("quiz-container");
-  const questionElement = document.getElementById("question");
-  const choicesElement = document.getElementById("choices");
-  const resultElement = document.getElementById("result");
-  const timeElement = document.getElementById("time");
-  
-  let currentQuestionIndex = 0;
-  let time = 0;
-  let timerInterval;
-  let score = 0;
-  let highScores = [];
-  
-  // Start the quiz
-  function startQuiz() {
-    time = 60;
-    timerInterval = setInterval(updateTimer, 1000);
-    showQuestion();
-  }
-  
-  // Update the timer display and check if time is up
-  function updateTimer() {
-    time--;
-    timeElement.textContent = time;
-  
-    if (time <= 0) {
-      endQuiz();
-    }
-  }
-  
-  // Show the current question
-  function showQuestion() {
-    const question = questions[currentQuestionIndex];
-    questionElement.textContent = question.question;
-    choicesElement.innerHTML = "";
-  
-    for (let i = 0; i < question.choices.length; i++) {
-      const choice = question.choices[i];
-      const button = document.createElement("button");
-      button.textContent = choice;
-      button.addEventListener("click", checkAnswer);
-      choicesElement.appendChild(button);
-    }
-  }
-  
-  // Check the user's answer
-  function checkAnswer(event) {
-    const selectedChoice = event.target;
-    const answer = questions[currentQuestionIndex].answer;
-  
-    if (selectedChoice.textContent === answer) {
-      resultElement.textContent = "Correct!";
-      score++;
-    } else {
-      resultElement.textContent = "Wrong!";
-      time -= 10; // Subtract 10 seconds for incorrect answers
-    }
-  
-    currentQuestionIndex++;
-  
-    if (currentQuestionIndex < questions.length) {
-      showQuestion();
-    } else {
-      endQuiz();
-    }
-  }
-  
-  // End the quiz
-  function endQuiz() {
-    clearInterval(timerInterval);
-    quizContainer.style.display = "none";
-    resultElement.textContent = "Quiz Over! Your score is: " + score;
-  
-    const initials = prompt("Enter your initials:");
-    highScores.push({ initials, score });
-  
-   // Show the scores
-function showScores() {
-    quizContainer.style.display = "none";
-    resultElement.textContent = "Quiz Over! Your score is: " + score;
-  
-    const initials = prompt("Enter your initials:");
-    highScores.push({ initials, score });
-  
-    // Sort the high scores in descending order
-    highScores.sort((a, b) => b.score - a.score);
-  
-    const scoresElement = document.getElementById("scores");
-    scoresElement.style.display = "block";
-    scoresElement.innerHTML = "<h2>High Scores</h2>";
-  
-    for (let i = 0; i < highScores.length; i++) {
-      const scoreEntry = document.createElement("p");
-      scoreEntry.textContent = `${highScores[i].initials}: ${highScores[i].score}`;
-      scoresElement.appendChild(scoreEntry);
-    }
-  
-    // Save the high scores to localStorage
-    localStorage.setItem("highScores", JSON.stringify(highScores));
-  }
+];
 
-  
-  // Restart the quiz
-  function restartQuiz() {
-    currentQuestionIndex = 0;
-    time = 60;
-    score = 0;
-    resultElement.textContent = "";
-    startQuiz();
+// Quiz variables
+const quizContainer = document.getElementById("quiz-container");
+const questionElement = document.getElementById("question");
+const choicesContainer = document.getElementById("choices");
+const submitButton = document.getElementById("submit");
+const timerElement = document.getElementById("time");
+const scoreContainer = document.getElementById("score-container");
+const scoreElement = document.getElementById("score");
+const initialsForm = document.getElementById("initials-form");
+const initialsInput = document.getElementById("initials");
+
+let currentQuestionIndex = 0;
+let time = 60;
+let score = 0;
+let timerInterval;
+
+// Event listener for starting the quiz
+submitButton.addEventListener("click", startQuiz);
+
+// Function to start the quiz
+function startQuiz() {
+  quizContainer.style.display = "block";
+  submitButton.style.display = "block";
+  timerInterval = setInterval(updateTimer, 1000);
+  displayQuestion();
+}
+
+// Function to display a question
+function displayQuestion() {
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+  questionElement.textContent = currentQuestion.question;
+  choicesContainer.innerHTML = "";
+
+  for (let i = 0; i < currentQuestion.choices.length; i++) {
+    const choice = currentQuestion.choices[i];
+    const choiceButton = document.createElement("button");
+    choiceButton.textContent = choice;
+    choiceButton.addEventListener("click", function() {
+      checkAnswer(choice);
+    });
+    choicesContainer.appendChild(choiceButton);
   }
-  
-  // Event listener for the start button
-  document.addEventListener("DOMContentLoaded", function() {
-    const startButton = document.createElement("button");
-    startButton.textContent = "Start";
-    startButton.addEventListener("click", startQuiz);
-    quizContainer.appendChild(startButton);
-  });
-  
-  // Save the high scores to localStorage
-  localStorage.setItem("highScores", JSON.stringify(highScores));
-  }  
+}
 
+// Function to check the selected answer
+function checkAnswer(choice) {
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+  if (choice === currentQuestion.answer) {
+    score += 10;
+  } else {
+    time -= 10;
+    if (time < 0) {
+      time = 0;
+    }
+  }
+  currentQuestionIndex++;
 
-  
-  
+  if (currentQuestionIndex === quizQuestions.length || time === 0) {
+    endQuiz();
+  } else {
+    displayQuestion();
+  }
+}
+
+// Function to update the timer
+function updateTimer() {
+  timerElement.textContent = time;
+  time--;
+
+  if (time < 0) {
+    endQuiz();
+  }
+}
+
+// Function to end the quiz
+function endQuiz() {
+  clearInterval(timerInterval);
+  quizContainer.style.display = "none";
+  submitButton.style.display = "none";
+  scoreElement.textContent = score;
+  scoreContainer.classList.remove("hide");
+}
+
+// Event listener for submitting initials
+initialsForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  saveScore();
+});
+
+// Function to save the score and initials
+function saveScore() {
+  const initials = initialsInput.value;
+  // Code to save the score here
+  console.log(`Score saved: Initials - ${initials}, Score - ${score}`);
+  initialsForm.reset();
+}
